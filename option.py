b76945beb077d9defaf8be6caeb1bc96e5310d85
Lucien <lucien1011@gmail.com>
2015-11-28 16:03:27 +0100

    first scripts to read post given a username

---
+++ option.py
@@ -0,0 +1,8 @@
+
+import argparse
+
+#____________________________________________________________________________||
+
+parser = argparse.ArgumentParser(description='Argument Parser for Facebook')
+
+parser.add_argument('--account', action='store', dest='account',help="log in account")
