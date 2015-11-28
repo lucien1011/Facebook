6c18099ce83767d52f41c882292f79931d3b0b75
Lucien <lucien1011@gmail.com>
2015-11-28 22:46:40 +0100

    a script to write to my wall

--- writeMyWall.py
+++ writeMyWall.py
@@ -1,20 +1,11 @@
-"""
-A simple example script to get all posts on a user's timeline.
-Originally created by Mitchell Stewart.
-<https://gist.github.com/mylsb/10294040>
-"""
+
 import facebook
 import requests
 from option import parser
 
 #____________________________________________________________________________||
 
-def some_action(post):
-    """ Here you might want to do something with each post. E.g. grab the
-    post's message (post['message']) or the post's picture (post['picture']).
-    In this implementation we just print the post's created time.
-    """
-    print(post['created_time'])
+parser.add_argument('--message', action = 'store', dest = 'message', help = 'message to be sent')
 
 #____________________________________________________________________________||
 
@@ -22,31 +13,17 @@ args = parser.parse_args()
 
 #____________________________________________________________________________||
 
-account = args.account
+token = args.token
+wallMessage = args.message
 
 #____________________________________________________________________________||
 
-
-# You'll need an access token here to do anything.  You can get a temporary one
-# here: https://developers.facebook.com/tools/explorer/
-access_token = ''
-# Look at Bill Gates's profile for this example by using his Facebook id.
-user = account
+# https://developers.facebook.com/tools/explorer/
+access_token = token
+user = "me"
 
 graph = facebook.GraphAPI(access_token)
 profile = graph.get_object(user)
-posts = graph.get_connections(profile['id'], 'posts')
+posts = graph.get_connections(user, 'friends')
 
-# Wrap this block in a while loop so we can keep paginating requests until
-# finished.
-while True:
-    try:
-        # Perform some action on each post in the collection we receive from
-        # Facebook.
-        [some_action(post=post) for post in posts['data']]
-        # Attempt to make a request to the next page of data, if it exists.
-        posts = requests.get(posts['paging']['next']).json()
-    except KeyError:
-        # When there are no more pages (['paging']['next']), break from the
-        # loop and end the script.
-        break
+graph.put_object( user , "feed", message = wallMessage )
